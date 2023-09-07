bc72b6684ed66de0b22bec5b5003e45ae6a14d39
victorpreston <prestonvictor25@gmail.com>
2023-09-07 03:41:10 -0700

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,7 +2,8 @@
 
 def magic_calculation(a, b):
     """Match bytecode provided"""
-    from magic_calculation_102 import add, sub
+    add = __import__('magic_calculation_102').add
+    sub = __import__('magic_calculation_102').sub
 
     if a < b:
         c = add(a, b)
