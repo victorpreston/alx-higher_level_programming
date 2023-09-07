2ac9965a0125904f1732f5aeed83d38cd129edf5
victorpreston <prestonvictor25@gmail.com>
2023-09-07 03:02:11 -0700

    4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Print names defined in the hidden_4 module"""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
