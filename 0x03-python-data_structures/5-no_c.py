4780fd198931a1dd8396d282d64e5d2352113884
victorpreston <prestonvictor25@gmail.com>
2023-09-11 17:38:55 -0700

    5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    """Function that removes all c and C characters from a string"""
+    new_string = ""
+    for j in range(len(my_string)):
+        if my_string[j] != 'c' and my_string[j] != 'C':
+            new_string += my_string[j]
+    return new_string
