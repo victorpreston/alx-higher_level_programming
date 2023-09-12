fe84a1275d6a04a772fe97d5b07321e5563f63f2
victorpreston <prestonvictor25@gmail.com>
2023-09-11 17:31:53 -0700

    4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    """Replace an element at specific point[index] without modifying entire list"""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return my_list
+
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return copy
