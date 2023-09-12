c0de02943a9dc67b3bd3f6b35e7e810c8a461a22
victorpreston <prestonvictor25@gmail.com>
2023-09-11 17:34:34 -0700

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    """Replace an element at specific point[index] without modifying entire list"""
+    """Replace elmt at specific point without modifying entire list"""
     if idx < 0 or idx > (len(my_list) - 1):
         return my_list
 
