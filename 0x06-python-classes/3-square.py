b33f50e312b84db03fbe08c82d255fe17007aabe
victorpreston <prestonvictor25@gmail.com>
2023-09-27 16:02:06 -0700

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defining a class square"""
+
+
+class Square():
+    """Class Square with proper validation"""
+
+    def __init__(self, size=0):
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
