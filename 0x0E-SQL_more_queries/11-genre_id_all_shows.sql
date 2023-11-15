ced06cdd6da2828f61d5749a5802a6bbc7d42609
victorpreston <prestonvictor25@gmail.com>
2023-11-15 14:15:22 -0800

    11-genre_id_all_shows.sql

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+--  a script that lists all shows contained in the database hbtn_0d_tvshows.
+SELECT tv_shows.title, tv_show_genres.genre_id
+  FROM tv_show_genres
+  RIGHT JOIN tv_shows
+  ON tv_shows.id = tv_show_genres.show_id
+  ORDER BY tv_shows.title, tv_show_genres.genre_id;
