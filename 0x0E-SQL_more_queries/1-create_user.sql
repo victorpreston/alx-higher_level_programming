4ccd0a66af93a44b22e849e0561fa2ab4cbb095d
victorpreston <prestonvictor25@gmail.com>
2023-11-15 13:58:55 -0800

    1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,6 @@
--- Creates the user user_0d_1 with all privileges.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
+-- a script that creates the MySQL server user user_0d_1.
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES 
+ON *.*
+TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
