2603b221619421423f9fb3f0f2cc871767436254
victorpreston <prestonvictor25@gmail.com>
2023-11-15 14:14:01 -0800

    10-genre_id_by_show.sql

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT tv_shows.title, tv_show_genres.genre_id
+  FROM tv_show_genres
+  INNER JOIN tv_shows
+  ON tv_shows.id = tv_show_genres.show_id
+  ORDER BY tv_shows.title, tv_show_genres.genre_id;
