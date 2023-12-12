5748c3b48baa58a5b853363308d1bec9f52eadc0
victorpreston <prestonvictor25@gmail.com>
2023-12-12 03:14:47 -0800

    javascript_objects_scopes_closures

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
+const newDict = {};
+for (const key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
+  }
+}
+console.log(newDict);
