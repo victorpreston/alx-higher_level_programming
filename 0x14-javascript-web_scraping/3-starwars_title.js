5018fe37126ce468650a97b30cc1746205c99689
Victor Preston <112781610+victorpreston@users.noreply.github.com>
2024-04-23 22:23:20 +0300

    update: 3-starwars_title.js & complete task

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-const id = process.argv[2];
-request('http://swapi.co/api/films/' + id + '/', function (error, response, body) {
-  if (error == null) {
-    const json = JSON.parse(body);
-    console.log(json.title);
+const { argv } = require('process');
+
+const BaseUrl = 'https://swapi-api.hbtn.io/api';
+request(BaseUrl + '/films/' + argv[2], (error, response, body) => {
+  if (error) {
+    console.error(error);
   }
+  console.log(JSON.parse(body).title);
 });
