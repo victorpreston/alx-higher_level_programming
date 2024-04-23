033a4001ac3ced192e560cc30430f62ad8aaf5d8
Victor Preston <112781610+victorpreston@users.noreply.github.com>
2024-04-23 22:33:16 +0300

    completed: update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,7 +3,7 @@ const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (!error) {
     const todos = JSON.parse(body);
-    let completed = {};
+    const completed = {};
     todos.forEach((todo) => {
       if (todo.completed && completed[todo.userId] === undefined) {
         completed[todo.userId] = 1;
